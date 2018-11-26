b286128c407c40de3d265ad46feb3c48ee2e1a1d
Alefe Mafra <alefemafra@MacBook-Pro-de-Alefe.local>
2018-11-26 12:15:13 -0200

    Fix Error

---
+++ Alefe/FixDateProductError/registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Copyright © Hibrido. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Alefe_FixDateProductError',
+    __DIR__
+);
